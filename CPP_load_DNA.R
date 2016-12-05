129935a44f60f46201bb69aeca5d452b69af964f
Rebecca Krasnoff <rebecca.krasnoff@ucsf.edu>
2016-12-05 14:20:39 -0800

    Added function for translating to AAs. Gives option to translate from either RNA or DNA; I’m not sure if it correctly translates from DNA. Does it make choice of how to translate automatically based on class of data? Need to double check.

--- CPP_load_DNA.R
+++ CPP_load_DNA.R
@@ -8,6 +8,13 @@ names(test)
 
 s = readDNAStringSet("~/Downloads/Mus_musculus.GRCm38.cdna.all.fa")
 subseq(s, start=c(1, 2, 3), end=c(3, 6, 5))
+consensusMatrix(s, baseOnly=TRUE)
 
 df <- subseq(s)
 head(df)
+
+ambiguityMap <- (c("AGTC"))
+names(ambiguityMap) <- "N"
+
+testAA <- translate(s, genetic.code=GENETIC_CODE, if.fuzzy.codon="solve")
+head(testAA)
